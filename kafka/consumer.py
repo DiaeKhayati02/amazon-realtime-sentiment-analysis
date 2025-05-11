5d61a67882168c2ebab2002460d598be133204fc
Diaekhayati <khayatidiae67@gmail.com>
2025-05-11 10:51:31 +0100

    testing simple producer and consumer

---
+++ kafka/consumer.py
@@ -0,0 +1,19 @@
+import json
+from kafka import KafkaConsumer
+
+# Initialisation du consommateur Kafka
+consumer = KafkaConsumer(
+    'reviews',
+    #bootstrap_servers=['localhost:9092'],  # ou 'kafka:9092' si dans Docker
+    bootstrap_servers=['kafka:9093'],
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='test-consumer-group',
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+)
+
+print("📥 En attente de messages depuis le topic 'amazon-reviews'...")
+for message in consumer:
+    review = message.value
+    # print(f"Reçu : {review.get('reviewText', '')[:80]}...")
+    print(f"Reçu : {json.dumps(review, indent=2, ensure_ascii=False)}")
---
+++ kafka/producer.py
@@ -0,0 +1,31 @@
+import json
+import time
+from kafka import KafkaProducer
+
+DATA_FILE = "data/Musical_Instruments_5.json"
+
+# Initialisation du producteur Kafka
+producer = KafkaProducer(
+    # bootstrap_servers=['127.0.0.1:9092'],  # ou 'kafka:9092' si dans Docker
+    bootstrap_servers=['kafka:9093'],
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+TOPIC_NAME = "reviews"
+
+def stream_reviews():
+    with open(DATA_FILE, 'r', encoding='utf-8') as f:
+        for line in f:
+            try:
+                review = json.loads(line.strip())
+                producer.send(TOPIC_NAME, value=review)
+                # print(f"Message envoyé : {review.get('reviewText', '')[:80]}...")
+                print(f"Message envoyé : {json.dumps(review, indent=2, ensure_ascii=False)}...")
+                time.sleep(1)  # Simule un flux en temps réel
+            except json.JSONDecodeError:
+                print("error")
+                continue
+
+if __name__ == "__main__":
+    print("📤 Streaming des avis Amazon vers Kafka...")
+    stream_reviews()
